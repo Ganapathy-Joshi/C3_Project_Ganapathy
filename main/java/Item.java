324355f5daf777074a27467251e50ac868c35e58
Ganapathy-Joshi <joshiganapathys@gmail.com>
2021-01-10 21:03:06 +0000

    Part 3: Solution

--- main/java/Item.java
+++ main/java/Item.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class Item {
     private String name;
     private int price;
-    
+
+    private ArrayList<Item> items = new ArrayList<Item>(); 
+
 
     public Item(){};
 
@@ -28,6 +30,25 @@ public class Item {
                 ;
     }
 
+public void addItems(Item item)
+    {
+        items.add(item);
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public double getSelectedItemsTotalCost()
+    {
+        double totalCost = 0;
+        for(Item item : items)
+        {
+            totalCost += item.price;
+        }
+        return totalCost;
+    }
+    
    
 
 
